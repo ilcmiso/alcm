0baff68f921a4afa56f8ec822bbceb746bd13f5b
omiso-note <pirukand@gmail.com>
2025-02-07 10:51:49 +0900

    アプリfontをNoto Sans JPに統一

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'ui_fplan.dart'; // UI部分を別ファイルに分割
+import 'ui_fplan.dart';
 
 void main() {
-  // アプリを起動するエントリーポイント
-  runApp(const MaterialApp(
-    home: PropertyInputForm(), // UIを表示
+  runApp(MaterialApp(
+    theme: ThemeData(
+      fontFamily: 'NotoSansJP',
+    ),
+    home: PropertyInputForm(),
   ));
 }
